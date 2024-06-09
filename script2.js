c1da15dad51421fb4ce0145b80e983280a884110
Kardo Mohammed <158830224+7Kardo@users.noreply.github.com>
2024-06-09 15:46:20 +0200

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,11 +1,18 @@
 
-var counter = 0, num = 0;
-for(var i = 1;i<= num; i++){
-    if(num%i == 0)
-        counter++;
-}
-if(counter==2)
-    console.log('val');
-else{
-    console.log('morakab');
+// var counter = 0, num = 0;
+// for(var i = 1;i<= num; i++){
+//     if(num%i == 0)
+//         counter++;
+// }
+// if(counter==2)
+//     console.log('val');
+// else{
+//     console.log('morakab');
+// }
+for(var a = 1; a <= 2 ; a++ ){
+    console.log('outer'+a);
+    for( var b = 1; b<=3 ; b++){
+        console.log('inner'+b);
+    }
+
 }
